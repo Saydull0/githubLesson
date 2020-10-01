8e309c8958d0b2b42b7333e0ef12684807a3797d
Saydullo <s.toshtanoff@gmail.com>
2020-10-01 14:34:49 +0500

    from service

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Github Lesson");
-
+        System.out.println("Change from service");
         System.out.println("Change");
     }
 }
